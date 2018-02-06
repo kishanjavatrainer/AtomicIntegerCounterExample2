a1710c4ba4595cbeac6d1149dc7f1c8ad9d2ea34
Kishan Kumar <Kishan.javatrainer@gmail.com>
2018-02-06 09:43:22 +0530

    code

---
+++ AtomicIntegerCounterExample/src/com/infotech/count/Counter.java
@@ -0,0 +1,15 @@
+package com.infotech.count;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Counter {
+    private AtomicInteger counter = new AtomicInteger(0);
+ 
+    public int getValue(){
+        return counter.get();
+    }
+ 
+    public   int increment(){
+        return counter.getAndIncrement();
+    }
+}
